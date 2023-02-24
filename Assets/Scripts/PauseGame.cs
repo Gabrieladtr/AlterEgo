cf1d9aebb9d41d1f5244242e9fdef35681fa5c97
Gabrieladtr <118777502+Gabrieladtr@users.noreply.github.com>
2023-02-24 03:19:20 -0300

    Alter Ego - v06.2 - Links funcionando!
    ---------------------Tarefas de Hoje---------------------
    
    Resumo: Hoje consegui resolver todos os problemas que tive nos links. Agora todos os botões estão levando o jogador para a cena certa, sem haver problemas. Tmb consegui resolver o problema do Esc que quando era apertado em qualquer outro momento (tela aditivas), ele ativava. Agora não mais. Então, nem os links e nem o menu de pause estão problemáticos. A única coisa que me incomoda é o flood que é gerado no console quando eu invoco cenas aditivas pelo menu de pause, mas infelizmente eu preciso de um componente que gera esse flood que é o event system. Sem ele os links não funcionam, então eh uma mão pela outra. Enfim, consegui ser produtivo hoje!. Faltam poucas coisas para a interface ficar 100%. Assim que eu acabar isso, pretendo começar o sistema de resolução.
    
    -> Commit v06.2
    
    -> Resolver o problema dos links dos botões do pause. Se eu estiver dentro do pause, e clicar em algum dos outros botoes que leva para outra página, nao posso deixar o link delas me levar para o menu inicial. Quando eu aperto em voltar, elas devem me levar para a cena aberta anteriormente (se for menu inicial, volta para o menu inicial e se for o pause, deve voltar para o pause). Ou seja, preciso de um sistem que identifica em qual cena estamos e para qual devemos retornar. (Resolvido)
    
    ->Se eu apertar o esc dentro da tela de saving, ele pausa. E isso não deve acontecer. Tenho que criar um sistema que identifica em qual cena estamos e para qual vamos. Se essa lógica der certo, vamos poder controlar o sistema de pause e links de uma forma que não vai dar bugs. Isso é um problema de quando vc abre uma tela aditiva. Sempre que uma tela aditiva for aberta, ela é ignorada e a gente pensa que o código está funcionando mas nem... Se eu conseguir identificar qual cena aditiva está ativa eu consigo resolver esse problema. Eu preciso de um método que verifica se temos alguma cena aditiva criada. (Resolvido)
    
    ---------------------Problemas de hoje---------------------
    
    ->Estou criando um metodo que consegue verificar qual a cena anterior e qual a proxima. Dessa forma, conseguirei resolver o problema dos links. Eu já consigo saber quais as cenas que são anteriores e quais as que são as próximas. O problema? O método 'SceneManager.GetActiveScene().name' não retorna as cenas que foram abertas de forma aditiva. Sim, que B.O. do kct. (resolvido)
    
    ---------------------Pendentes de hoje---------------------
    
    ->Criar um botao que volta para o menu principal e verifica se o jogador salvou os dados. Se sim, volta caso não ele apenas faz uma verificação se o jogador quer salvar ou não. Já criei o botão e o link, mas ele ainda não verifica os dados salvos do jogador.
    
    ->Quando eu entro no pause ou em outra tela, e clico em qualquer coisa o jogo identifica como se eu estivesse jogando e avança os diálogos. Eu não posso deixar isso acontecer. Eu já pesquisei, existe um metodo que pausa o que estiver rolando em uma cena. Eu devo deixar isso ser aplicado quando eu entro no pause.
    
    -> Após o sistema de save e load estiverem 100% funcionais e sem bugs, quero começar a construir o sistema de resolução do jogo. A partir desse sistema eu quero identificar automaticamente qual a resolução do jogador, para ele não se preocupar com isso.
    
    -> Criar uma forma de carregar os dados do jogador no jogo, dentro da UI de Load.

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -8,7 +8,7 @@ public class PauseGame : MonoBehaviour
     //Essa classe controla se o pause esta ativado ou nao
     //Eu ativei essa classe dentro do background da cena do game. Dessa forma, o pause so funcina dentro do jogo.
 
-    public bool pauseActive = false;    
+    public static bool pauseActive = false;    
 
 
     
@@ -17,13 +17,14 @@ public class PauseGame : MonoBehaviour
 
         //controlamos se o pause esta ativo ou nao
 
-        if (Input.GetKeyDown(KeyCode.Escape) && pauseActive == false)
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseActive == false && SceneManager.GetActiveScene().name == "Game" && StartGame.CenaAditivaAtual == null)
         {
             Debug.Log("O menu de pausa deve ser aberto.");
             SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+            Debug.Log("Cena atual: " + SceneManager.GetActiveScene().name);
             pauseActive = true;
 
-        } else if (Input.GetKeyDown(KeyCode.Escape) && pauseActive == true)
+        } else if (Input.GetKeyDown(KeyCode.Escape) && pauseActive == true && SceneManager.GetActiveScene().name == "Game" && StartGame.CenaAditivaAtual == null)
         {
 
             Debug.Log("O menu de pausa deve ser fechado.");
@@ -33,6 +34,24 @@ public class PauseGame : MonoBehaviour
 
         }
 
+        DesativaPauseNoMenuPrincipal();
 
     }
+
+
+
+    public void DesativaPauseNoMenuPrincipal()
+    {
+        //Quando vamos para o menu principal a partir do menu de pause, vai ser ativado esse metodo
+        //dentro do background do menu principal que desativa o pauseActive permitindo que o jogador possa apertar Esc quantas vezes quiser.
+        if(pauseActive == true && SceneManager.GetActiveScene().name != "Game")
+        {
+
+            Debug.Log("O menu de pausa deve ser fechado a partir do menu principal.");
+            pauseActive = false;
+
+        }
+    }
+
+
 }
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -6,22 +6,30 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour
 {
     
-    
+    public static string CenaAtual;
+    public static string cenaAnteriorString, CenaAtualString;
+    public static bool DeveRetornarAoPause = false;
+    public static string CenaAditivaAtual = null;
+
 
 
     public void CloseScene(string stageToClose)
     {
+        //vai fechar uma cena.
         SceneManager.UnloadSceneAsync(stageToClose, UnloadSceneOptions.None);
         
-    }   
-
+    }
+  
 
     public void selectScene(string stageToPlay)
     {
+        //identifica a cena atual e a anterior e se o pause esta ativo
+        identificaCenaAtualEAnterior(stageToPlay);
+        //vai transportar o jogador para a proxima cena.
         SceneManager.LoadScene(stageToPlay);
+        
 
-
-        //Deve chamar a classe Game e inputar true no bool 'escolhaDialogicaBool' que adiciona um valor padrão em 'escolhaDialogica'
+        //Deve chamar a classe Game e inputar true no bool 'escolhaDialogicaBool' que adiciona um valor padrao em 'escolhaDialogica'
         //Isso significa que toda vez que o jogador iniciar o jogo no botao 'iniciar', ele vai iniciar com uma frase padrao
         //Sem gerar exceptions, multiplicar as frases que devem aparecer ou flodar o console.
 
@@ -37,9 +45,90 @@ public class StartGame : MonoBehaviour
 
     public void selectSceneAdditive(string stageToPlay)
     {
+        //vai abrir uma cena em cima de outra.
+
+        //identifica a cena atual, a anterior e se o pause esta ativo
+        identificaCenaAtualEAnterior(stageToPlay);
+        //carrega a proxima cena de forma aditiva (por cima)
         SceneManager.LoadScene(stageToPlay, LoadSceneMode.Additive);
+        //Vai dizer para a variavel de instancia qual cena aditiva foi carregada no momento.
+        CenaAditivaAtual = stageToPlay;
+
+
     }
 
 
+    //Preciso de um metodo que identifica qual cena estamos e para qual cena vamos 
+    //ou qual cena estamos e qual foi a cena anterior.
 
+    //se a cena atual for == a cena que vamos ir, entao o link vai nos levar para a msm cena.
+    //se a cena que estou eh diferente da cena que vamos ir, entao a cena atual sera a cena anterior
+    //e a cena que vamos ir sera a cena atual.
+
+    public void identificaCenaAtualEAnterior(string proximaCena)
+    {
+        cenaAnteriorString = SceneManager.GetActiveScene().name;
+        CenaAtualString = proximaCena;
+
+        if(cenaAnteriorString != proximaCena && PauseGame.pauseActive == false)
+        {
+            //Vai verificar se o pause esta ativo e qual a cena anterior e a proxima cena
+            Debug.Log("A cena anterior eh: " + cenaAnteriorString + ". A cena atual agora eh: " + CenaAtualString + ". O pause esta: " + PauseGame.pauseActive);
+            
+
+        }else if(cenaAnteriorString != proximaCena && PauseGame.pauseActive == true)
+        {
+            //Vai verificar se o pause esta ativo e qual a cena anterior e a proxima cena
+            Debug.Log("A cena anterior eh: " + cenaAnteriorString + ". A cena atual agora eh: " + CenaAtualString + ". O pause esta: " + PauseGame.pauseActive);
+            
+        }
+        
+        else if(cenaAnteriorString == proximaCena){
+
+            Debug.Log("A cena anterior eh a mesma que a proxima");
+        }else
+        {
+            Debug.Log("A cena atual eh totalmente diferente da proxima cena. Verificar.");
+        }
+
+    }
+
+    public void RetornarCenaAnterior(string cenaVoltar)
+    {
+        //Esse metodo recebe uma cena como padrao, para onde ela deva ir normalmente.
+        //Mas se por acaso o pause menu for aberto nesse caminho, ela deve retornar para essa cena.
+        //Ou seja, controlamos 2 funcoes nesse metodo. 
+        //Se a cena anterior for 'menu principal' e a proxima cena for 'como jogar', no botao de voltar devera voltar para 'menu principal'.
+        //Se a cena anterior for 'Pause' e a proxima cena for 'como jogar', no botao de voltar devera voltar para 'Pause'.
+        //Tudo em um mesmo botao.
+        
+
+        //Esse metodo deve controlar apenas o botao de voltar. Ele deve conseguir fechar uma cena aditiva e retornar para a cena anterior, usando o mesmo botao
+
+        if(PauseGame.pauseActive == true)
+        {
+            //se eu tiver que retonar ao pause, devo apenas fechar a janela atual aberta de forma aditiva
+            //Assim automaticamente ja estaremos dentro de Pause. Pq eh a cena anterior.
+            SceneManager.UnloadSceneAsync(CenaAtualString, UnloadSceneOptions.None);
+
+            
+            //Enquanto essa variavel tiver valor, toda vez que uma tela for carregada de forma aditiva o pause nao podera ser invocado.
+            //Atribuo para o 'CenaAditivaAtual' o valor null, pq dessa forma sendo ele null vai permitir que o menu de pause seja ativado.
+            CenaAditivaAtual = null;
+
+        }else if(PauseGame.pauseActive == false)
+        {
+            //Se o pause nao esta ativo, nos devemos apenas carregar a cena anterior.
+            SceneManager.LoadScene(cenaVoltar);
+        }else{
+            Debug.Log("Verifique o metodo RetornarCenaAnterior().");
+        }
+
+    }
+
+
+    
 }
+
+
+
